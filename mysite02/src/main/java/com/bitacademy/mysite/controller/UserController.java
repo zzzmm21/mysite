c796128961e87fd231a61bc941a6bcf13697670c
zzzmm21 <zzzmm21@gmail.com>
2022-11-09 14:58:39 +0900

    my site upload

---
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -0,0 +1,77 @@
+package com.bitacademy.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bitacademy.mysite.dao.UserDao;
+import com.bitacademy.mysite.vo.UserVo;
+
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+				.forward(request, response);
+		} else if("join".equals(action)){
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			
+			UserVo vo = new UserVo();
+			vo.setName(name);
+			vo.setEmail(email);
+			vo.setPassword(password);
+			vo.setGender(gender);
+
+			new UserDao().insert(vo);
+
+			response.sendRedirect(request.getContextPath() + "/user?a=joinsuccess");
+			
+		} else if("joinsuccess".equals(action)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
+				.forward(request, response);
+		} else if("loginform".equals(action)) {
+			request
+			.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+			.forward(request, response);
+		} else if("login".equals(action)) {
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+
+			UserDao dao = new UserDao();
+			UserVo authUser = dao.findByEmailAndPassword(email, password);
+			
+			if(authUser == null) {
+				/* 인증 실패 */
+				request.setAttribute("email", email);
+				request
+					.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp")
+					.forward(request, response);
+				return;
+			}
+
+			/* 로그인 처리 */
+			HttpSession session = request.getSession(true);
+			session.setAttribute("authUser", authUser);			
+			response.sendRedirect(request.getContextPath());
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
--- mysite02/src/main/webapp/WEB-INF/views/user/joinsuccess.jsp
+++ mysite02/src/main/webapp/WEB-INF/views/user/joinsuccess.jsp
@@ -1,42 +1,25 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <title>mysite</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
-<link href="/assets/css/user.css" rel="stylesheet" type="text/css">
+<link href="<%=request.getContextPath() %>/assets/css/user.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<div id="container">
-		<div id="header">
-			<h1>MySite</h1>
-			<ul>
-				<li><a href="">로그인</a><li>
-				<li><a href="">회원가입</a><li>
-				<li><a href="">회원정보수정</a><li>
-				<li><a href="">로그아웃</a><li>
-				<li>님 안녕하세요 ^^;</li>
-			</ul>
-		</div>
+		<jsp:include page="/WEB-INF/views/includes/header.jsp"/>
 		<div id="content">
 			<div id="user">
 				<p class="jr-success">
 					회원가입을 축하합니다.
 					<br><br>
-					<a href="/user?a=loginform">로그인하기</a>
+					<a href="<%=request.getContextPath() %>/user?a=loginform">로그인하기</a>
 				</p>				
 			</div>
 		</div>
-		<div id="navigation">
-			<ul>
-				<li><a href="">안대혁</a></li>
-				<li><a href="">방명록</a></li>
-				<li><a href="">게시판</a></li>
-			</ul>
-		</div>
-		<div id="footer">
-			<p>(c)opyright 2015, 2016, 2017, 2018</p>
-		</div>
+		<jsp:include page="/WEB-INF/views/includes/navigation.jsp"/>
+		<jsp:include page="/WEB-INF/views/includes/footer.jsp"/>
 	</div>
 </body>
 </html>
