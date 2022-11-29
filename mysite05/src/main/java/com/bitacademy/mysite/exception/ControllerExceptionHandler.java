71599c0e4ce65c463ae68d25118ce1c211078963
zzzmm21 <zzzmm21@gmail.com>
2022-11-29 14:12:05 +0900

    mysite05 #finish

--- mysite05/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
+++ mysite05/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,6 +16,11 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public String HandlerException(Model model, Exception e) {
+		//404Error
+		if(e instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+		//500 Error 처리
 		// 로깅
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
