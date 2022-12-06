15a098414f66df229664cfd5b443d8d388f964b4
zzzmm21 <zzzmm21@gmail.com>
2022-12-06 14:59:47 +0900

    mysite05: web.xml 없애기

---
+++ mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
@@ -0,0 +1,39 @@
+package com.bitacademy.mysite.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class MVCConfig implements WebMvcConfigurer {	
+	// View Resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/assets/**")
+			.addResourceLocations("classpath:/assets/");
+	}
+	
+	
+	// 서블릿 컨테이너의 디폴트 서블릿 위임 핸들러
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
+}
---
+++ mysite05/src/main/java/com/bitacademy/mysite/initializer/MySiteWebApplicationinitializer.java
@@ -0,0 +1,47 @@
+package com.bitacademy.mysite.initializer;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.bitacademy.mysite.config.AppConfig;
+import com.bitacademy.mysite.config.WebConfig;
+
+public class MySiteWebApplicationinitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+	
+
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new CharacterEncodingFilter("UTF-8", false)};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet servlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+		servlet.setThrowExceptionIfNoHandlerFound(true);
+		return servlet;
+	}
+	
+	
+
+}
