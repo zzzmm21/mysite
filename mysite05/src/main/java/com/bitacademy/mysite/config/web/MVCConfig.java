31217a2964d86677420e539342fcb3b3b7aec11b
zzzmm21 <zzzmm21@gmail.com>
2022-12-06 15:14:07 +0900

    베포

--- mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
+++ mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
@@ -3,7 +3,6 @@ package com.bitacademy.mysite.config.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
